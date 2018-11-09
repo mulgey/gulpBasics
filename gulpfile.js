02ed1dd62511964a830eba2718d13b5a35497bb2
knh369 <knh369@konyadh.gov.tr>
2018-11-09 12:04:56 +0300

    daily edu

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 'use strict'; // Başlangıçta zorunlu diyebiliriz
 
 var gulp = require("gulp");
+var concat = require("gulp-concat");
 
-gulp.task("hello", function() {
-    console.log("Selam dostum");
+gulp.task("concatJScripts", function() {
+    gulp.src([
+        'js/jquery.js',
+        'js/sticky/jquery.sticky.js',
+        'js/main.js'])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"));
 });
 
-gulp.task("default", ["hello"], function() {
+gulp.task("default", ["concatJScripts"], function() {
     console.log("Bu standart oldu"); // Fonksiyon tanımlamazsak eğer, sadece hello yu çalıştırıp geçer
 });
 
